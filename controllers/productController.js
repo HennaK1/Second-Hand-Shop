0e26499901c0584ea9904e8c4ee8de70dc265a44
johannes <johannes.jokinen@metropolia.fi>
2021-12-08 15:26:39 +0200

    models, controllers and sql

---
+++ controllers/productController.js
@@ -0,0 +1,161 @@
+"use strict";
+const { validationResult } = require("express-validator");
+// productController
+const {
+  getAllProducts,
+  getProduct,
+  addProduct,
+  deleteProduct,
+} = require("../models/productModel");
+const { httpError } = require("../utils/errors");
+const { makeThumbnail } = require("../utils/resize");
+
+const product_list_get = async (req, res, next) => {
+  try {
+    const users = await getAllProducts(next);
+    if (users.length > 0) {
+      res.json(users);
+    } else {
+      next("No users found", 404);
+    }
+  } catch (e) {
+    console.log("product_list_get error", e.message);
+    next(httpError("internal server error", 500));
+  }
+};
+
+const product_get = async (req, res, next) => {
+  try {
+    const vastaus = await getProduct(req.params.id, next);
+    if (vastaus.length > 0) {
+      res.json(vastaus.pop());
+    } else {
+      next(httpError("No user found", 404));
+    }
+  } catch (e) {
+    console.log("product_get error", e.message);
+    next(httpError("internal server error", 500));
+  }
+};
+
+const product_post = async (req, res, next) => {
+  console.log("user_post", req.body, req.file, req.user);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("product_post validation", errors.array());
+    next(httpError("invalid data", 400));
+    return;
+  }
+
+  if (!req.file) {
+    const err = httpError("file not valid", 400);
+    next(err);
+    return;
+  }
+
+  try {
+    const thumb = await makeThumbnail(
+        req.file.path,
+        "./thumbnails/" + req.file.filename
+    );
+
+    const { Caption, CategoryId, ImageLocation, LikeCount, ProductId, Price, UserName } = req.body;
+
+    const result = await addProduct(
+        Caption,
+        ImageLocation,
+        req.user.user_id,
+        CategoryId,
+        req.file.filename,
+        LikeCount,
+        ProductId,
+        Price,
+        UserName,
+        next
+    );
+    if (thumb) {
+      if (result.affectedRows > 0) {
+        res.json({
+          message: "product added",
+          product_id: result.insertId,
+        });
+      } else {
+        next(httpError("No product inserted", 400));
+      }
+    }
+  } catch (e) {
+    console.log("product_post error", e.message);
+    next(httpError("internal server error", 500));
+  }
+};
+
+const product_put = async (req, res, next) => {
+  console.log("product_put", req.body, req.params);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("product_put validation", errors.array());
+    next(httpError("invalid data", 400));
+    return;
+  }
+  // pvm VVVV-KK-PP esim 2010-05-28
+  try {
+    const { Price, CategoryId, Caption } = req.body;
+    /*let owner = req.user.user_id;
+    if (req.user.role === 0) {
+      owner = req.body.owner;
+    }*/
+
+    const owner = req.user.role === 0 ? req.body.owner : req.user.user_id;
+
+    const result = await modifyProduct(
+        Caption,
+        CategoryId,
+        owner,
+        Price,
+        req.params.id,
+        req.user.role,
+        next
+    );
+    if (result.affectedRows > 0) {
+      res.json({
+        message: "product modified",
+        product_id: result.insertId,
+      });
+    } else {
+      next(httpError("No products modified", 400));
+    }
+  } catch (e) {
+    console.log("product_put error", e.message);
+    next(httpError("internal server error", 500));
+  }
+};
+
+const product_delete = async (req, res, next) => {
+  try {
+    const vastaus = await deleteProduct(
+        req.params.id,
+        req.user.user_id,
+        req.user.role,
+        next
+    );
+    if (vastaus.affectedRows > 0) {
+      res.json({
+        message: "product deleted",
+        product_id: vastaus.insertId,
+      });
+    } else {
+      next(httpError("No product found", 404));
+    }
+  } catch (e) {
+    console.log("product_delete error", e.message);
+    next(httpError("internal server error", 500));
+  }
+};
+
+module.exports = {
+  product_list_get,
+  product_get,
+  product_post,
+  product_delete,
+  product_put
+};
---
+++ models/productModel.js
@@ -0,0 +1,141 @@
+"use strict";
+const pool = require("../database/kirppis");
+const { httpError } = require("../utils/errors");
+const promisePool = pool.promise();
+
+const getAllProducts = async (next) => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = await promisePool.execute(`
+	SELECT 
+	Caption,
+	CategoryId,
+	Gps,
+	ImageLocation,
+	LikeCount,
+	Price,
+	ProductId,
+	UserName,
+	FROM Product
+	JOIN Category ON 
+	wop_product.owner = wop_user.user_id`);
+    return rows;
+  } catch (e) {
+    console.error("getAllProducts error", e.message);
+    next(httpError("Database error", 500));
+  }
+};
+
+const getProductsByKeyword = async (next) => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = await promisePool.execute(`
+	SELECT 
+  CategoryId,
+	CategoryName, 
+	FROM Category`);
+    return rows;
+  } catch (e) {
+    console.error("getProductsByKeyword error", e.message);
+    next(httpError("Database error", 500));
+  }
+};
+
+const getProduct = async (id, next) => {
+  try {
+    const [rows] = await promisePool.execute(
+        `
+	  SELECT 
+	  ProductId, 
+	  wop_product.name, 
+	  Gps, 
+	  owner, 
+	  ImageLocation,
+	  Price, 
+	  LikeCount,
+	  wop_user.name as ownername 
+	  FROM wop_product 
+	  JOIN wop_user ON 
+	  wop_product.owner = wop_user.user_id
+	  WHERE product_id = ?`,
+        [id]
+    );
+    return rows;
+  } catch (e) {
+    console.error("getProduct error", e.message);
+    next(httpError("Database error", 500));
+  }
+};
+
+const addProduct = async (
+    ProductLocation,
+    Caption,
+    UserName,
+    CategoryName,
+    next
+) => {
+  try {
+    const [rows] = await promisePool.execute(
+        "INSERT INTO tiedosto (ProductLocation, Caption, UserName) VALUES (?, ?, ?, ?, ?)",
+        [ProductLocation, Caption, UserName],
+        "INSERT INTO relationship (CategoryName, Tiedostonumero)VALUES (?, LAST_INSERT_ID()",
+        [CategoryName]
+    );
+    return rows;
+  } catch (e) {
+    console.error("addProduct error", e.message);
+    next(httpError("Database error", 500));
+  }
+};
+
+const modifyProduct = async (
+    ProductId,
+    Caption,
+    owner,
+    Gps,
+    Price,
+    role,
+    next
+) => {
+  let sql =
+      "UPDATE wop_product SET ProductId = ?, Caption = ?, Gps = ? WHERE Price = ? AND owner = ?;";
+  let params = [ProductId, Caption, Gps, Price, owner];
+  if (role === 0) {
+    sql =
+        "UPDATE wop_product SET ProductId = ?, Caption = ?, Gps = ?, owner = ? WHERE Price = ?;";
+    params = [ProductId, Caption, Gps, owner, Price];
+  }
+  console.log("sql", sql);
+  try {
+    const [rows] = await promisePool.execute(sql, params);
+    return rows;
+  } catch (e) {
+    console.error("addProduct error", e.message);
+    next(httpError("Database error", 500));
+  }
+};
+
+const deleteProduct = async (id, owner_id, role, next) => {
+  let sql = "DELETE FROM wop_product WHERE product_id = ? AND owner = ?";
+  let params = [id, owner_id];
+  if (role === 0) {
+    sql = "DELETE FROM wop_product WHERE product_id = ?";
+    params = [id];
+  }
+  try {
+    const [rows] = await promisePool.execute(sql, params);
+    return rows;
+  } catch (e) {
+    console.error("getProduct error", e.message);
+    next(httpError("Database error", 500));
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductsByKeyword,
+  getProduct,
+  addProduct,
+  modifyProduct,
+  deleteProduct,
+};
