61545ee56cda85b6f5a860b9bc7d3d661a36e965
johannes <johannes.jokinen@metropolia.fi>
2021-12-09 19:10:13 +0200

    models and controllers update
    utils and errors

---
+++ back/routes/productRoute.js
@@ -0,0 +1,43 @@
+"use strict";
+// productRoute
+const express = require("express");
+const { body } = require("express-validator");
+const multer = require("multer");
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.includes("image")) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({ dest: "./uploads/", fileFilter });
+const {
+  product_list_get,
+  product_get,
+  product_post,
+  product_put,
+  product_delete,
+} = require("../controllers/productController");
+const router = express.Router();
+
+router.route("/").get(product_list_get).post(
+    //passport.authenticate("jwt", { session: false }),
+    upload.single("product"),
+    body("name").notEmpty().escape(),
+    body("birthdate").isDate(),
+    body("weight").isNumeric(),
+    product_post
+);
+
+router
+.route("/:id")
+.get(product_get)
+.delete(product_delete)
+.put(
+    body("name").notEmpty().escape(),
+    body("birthdate").isDate(),
+    body("weight").isNumeric(),
+    product_put
+);
+
+module.exports = router;
