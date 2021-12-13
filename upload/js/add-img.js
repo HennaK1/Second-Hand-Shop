919785dd09bc08110b75a1c4ce781718392b60de
HennaK1 <henna.koli@metropolia.fi>
2021-12-13 19:39:25 +0200

    category

--- upload/js/add-img.js
+++ upload/js/add-img.js
@@ -1,20 +1,46 @@
 'use strict';
 const url = 'http://localhost:3000'; // change url when uploading to server
 
+const Categories = [
+    {
+        title: 'Paidat',
+    },
+    {
+        title: 'Mekot',
+    },
+    {
+        title: 'Takit',
+    },
+
+];
+
+const optionElement = document.querySelector(".add-category");
+
+optionElement.innerHTML = '';
+
+// lisätään tarvittava html-koodi, katso tarvittavat tagit esim. task-b.html
+for (let i= 0; i < Categories.length; i++){
+    optionElement.innerHTML +=
+        `<option>
+        ${Categories[i].title}
+        </option>`;
+}
+
+
 // select existing html elements
 const addForm = document.querySelector('#addProductForm');
 const categoryList = document.querySelector('.add-category');
 
 // create user options to <select>
-const createCategoryOptions = (users) => {
+const createCategoryOptions = (Categories) => {
     // clear user list
     categoryList.innerHTML = '';
-    users.forEach((user) => {
+    Categories.forEach((Category) => {
         // create options with DOM methods
         const option = document.createElement('option');
-        option.value = Category.CategoryId;
+        option.value = Category.CategoryName;
         option.innerHTML = Category.CategoryName;
-        option.classList.add('light-border');
+        option.classList.add('border');
         categoryList.appendChild(option);
     });
     // load cat data after users
@@ -22,7 +48,7 @@ const createCategoryOptions = (users) => {
 };
 
 // get users to make options
-const getCategory = async () => {
+const getCategories = async () => {
     try {
         const options = {
             headers: {
@@ -30,8 +56,8 @@ const getCategory = async () => {
             },
         };
         const response = await fetch(url + '/user', options);
-        const users = await response.json();
-        createCategoryOptions(users);
+        const Categories = await response.json();
+        createCategoryOptions(Categories);
     } catch (e) {
         console.log(e.message);
     }
