cc608692e312909248448df6c60fce47b793edd6
HennaK1 <henna.koli@metropolia.fi>
2021-12-13 16:32:58 +0200

    category

--- upload/js/add-img.js
+++ upload/js/add-img.js
@@ -3,9 +3,41 @@ const url = 'http://localhost:3000'; // change url when uploading to server
 
 // select existing html elements
 const addForm = document.querySelector('#addProductForm');
-const userList = document.querySelector('.add-user');
+const categoryList = document.querySelector('.add-category');
 
-// submit add cat form
+// create user options to <select>
+const createCategoryOptions = (users) => {
+    // clear user list
+    categoryList.innerHTML = '';
+    users.forEach((user) => {
+        // create options with DOM methods
+        const option = document.createElement('option');
+        option.value = Category.CategoryId;
+        option.innerHTML = Category.CategoryName;
+        option.classList.add('light-border');
+        categoryList.appendChild(option);
+    });
+    // load cat data after users
+    getProduct(ProductId);
+};
+
+// get users to make options
+const getCategory = async () => {
+    try {
+        const options = {
+            headers: {
+                Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+            },
+        };
+        const response = await fetch(url + '/user', options);
+        const users = await response.json();
+        createCategoryOptions(users);
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+
+// submit add product form
 addForm.addEventListener('submit', async (evt) => {
     evt.preventDefault();
     const fd = new FormData(addForm);
@@ -16,8 +48,8 @@ addForm.addEventListener('submit', async (evt) => {
         },
         body: fd,
     };
-    const response = await fetch(url + '/cat', fetchOptions);
+    const response = await fetch(url + '/product', fetchOptions);
     const json = await response.json();
     alert(json.message);
-    location.href = 'front.html';
+    location.href = '../front/front.html';
 });
