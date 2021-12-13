ad09235ad01a66a84dd93205f5da7cf3c543fce0
HennaK1 <henna.koli@metropolia.fi>
2021-12-13 22:20:52 +0200

    category

--- upload/js/add-img.js
+++ upload/js/add-img.js
@@ -3,46 +3,61 @@ const url = 'http://localhost:3000'; // change url when uploading to server
 
 //onst Categories = [
 //    {
- //       title: 'Paidat',
-  //  },
-  //  {
-  //      title: 'Mekot',
-  //  },
-  //  {
-  //      title: 'Takit',
- //   },
+//       title: 'Paidat',
+//  },
+//  {
+//      title: 'Mekot',
+//  },
+//  {
+//      title: 'Takit',
+//   },
 
 //];
 
 const optionElement = document.querySelector(".add-category");
 
-optionElement.innerHTML = '';
+//optionElement.innerHTML = '';
 
-for (let i= 0; i < Categories.length; i++){
-    optionElement.innerHTML +=
-        `<option>
-        ${Categories[i].title}
-        </option>`;
-}
+//for (let i= 0; i < Category.length; i++){
+    //optionElement.innerHTML +=
+        //`<option>
+        //${Category[i].CategoryName}
+        //</option>`;
+//}
 
 // select existing html elements
 const addForm = document.querySelector('#addProductForm');
 const categoryList = document.querySelector('.add-category');
 
 // create user options to <select>
-const createCategoryOptions = (Categories) => {
+const createCategoryOptions = (categories) => {
 
     categoryList.innerHTML = '';
-    Categories.forEach((Category) => {
+    categories.forEach((categories) => {
         // create options with DOM methods
         const option = document.createElement('option');
-        option.value = Category.CategoryName;
-        option.innerHTML = Category.CategoryName;
+        option.value = categories.CategoryName;
+        option.innerHTML = categories.CategoryName;
         option.classList.add('border');
         categoryList.appendChild(option);
     });
-    // load product data after users
+    // load product data after categories
     getProduct(ProductId);
+};
+// get categories to make options
+const getCategories = async () => {
+    try {
+        const options = {
+            headers: {
+                Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+            },
+        };
+        const response = await fetch(url + '/categories', options);
+        const categories = await response.json();
+        createCategoryOptions(categories);
+    } catch (e) {
+        console.log(e.message);
+    }
 };
 
 // submit add product form
@@ -64,9 +79,9 @@ addForm.addEventListener('submit', async (evt) => {
 //AJAX CALLS
 const getGategoryData = async () => {
     try {
-        const response = await fetch(url + '/recipe');
-        const recipes = await response.json();
-        getGategoryData(Categories);
+        const response = await fetch(url + '/product');
+        const categories = await response.json();
+        getGategoryData(categories);
     } catch (e) {
         console.log(e.message);
     }
@@ -74,9 +89,9 @@ const getGategoryData = async () => {
 
 const getCategories = async () => {
     try {
-        const response = await fetch(url + '/category');
+        const response = await fetch(url + '/product');
         const categories = await response.json();
-        getCategoryData(Categories);
+        getCategoryData(categories);
     } catch (e) {
         console.log(e.message);
     }
